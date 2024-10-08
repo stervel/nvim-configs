47daf2baea9bbf5ae7bef119b05b4b7d45e6bd0e
stervel <sevenorbs.mail@gmail.com>
2024-10-09 06:05:46 +0700

    vimtex config

--- lua/kickstart/plugins/vimtex.lua
+++ lua/kickstart/plugins/vimtex.lua
@@ -4,6 +4,11 @@ return {
 
     init = function()
       vim.g.vimtex_view_method = 'general'
+      vim.syntax = 'enable'
+      vim.g.tex_flavor = 'latex'
+      vim.g.vimtex_quickfix_mode = 0
+      vim.g.vimtex_format_enabled = 1
+      vim.g.vimtex_complete_enabled = 1
     end,
   },
 }
